d0b1e64c13e264b5bdecf579cfe4dca00e6dac37
James Stewart <jstewart101@gmail.com>
2017-09-08 11:45:54 +1000

    Add custom attributes to products
    Custom attributes are those which may be modified by a customer but are not
    part of a variant definition because they do not affect price or stock.

--- saleor/product/forms.py
+++ saleor/product/forms.py
@@ -40,3 +40,5 @@ class ProductForm(AddToCartForm):
     def get_variant(self, cleaned_data):
         return cleaned_data.get('variant')
 
+    def get_customizations(self, cleaned_data):
+        return json.loads(cleaned_data.get('customizations', '{}'))
